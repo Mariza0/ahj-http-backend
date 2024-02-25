74c3936e07292018e0cc488188a87fd0e48ed0fd
Mariza0 <maria.net@mail.ru>
2024-02-25 16:15:03 +0100

    fix server

--- server.js
+++ server.js
@@ -84,14 +84,10 @@ app.use((ctx, next) => {
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //     next();
 // });
-app.use((ctx, next) => {
-    ctx.response.set("Access-Control-Allow-Origin", "https://mariza0.github.io/ahj-ws-frontend/");
-    ctx.response.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// 
 
 app.use((ctx, next) => {
-    ctx.response.set("Access-Control-Allow-Origin", "http://localhost:8080/");
+    ctx.response.set('Access-Control-Allow-Origin', "https://mariza0.github.io, https://localhost:8080");
     ctx.response.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
